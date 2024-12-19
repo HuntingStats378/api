edacbadb4ae3ca6d15be1ff5273409c217c5e6a0
HuntingStats378 <140703804+HuntingStats378@users.noreply.github.com>
2024-12-19 17:13:09 +0000

    Update server.js

--- server.js
+++ server.js
@@ -105,3 +105,8 @@ app.get("/api/mrbeast/youtube-subcount/", async (req, res) => {
 });
 
 module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
