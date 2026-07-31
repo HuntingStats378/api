5ffb2863fc55044702515460b43ba9e352ed6ed0
HuntingStats378 <140703804+HuntingStats378@users.noreply.github.com>
2026-07-31 07:42:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -884,7 +884,7 @@ app.get("/api/twitter/user/:id", async (req, res) => {
 
 async function fetchyoutubeapivideo(videoId) {
   try {
-    const data = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=${videoId}&key=${YOUTUBE_API_KEY}`);
+    const data = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails,status&id=${videoId}&key=${YOUTUBE_API_KEY}`);
     const response = await data.json();
     return response;
   } catch (error) {
